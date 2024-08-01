4fd55b56b612b67a77e5db008f1db51bf2236f6f
Yader Campbell <yadermejiac123@gmail.com>
2024-08-01 00:16:02 -0600

    feat: Rest endpoint - Create user

---
+++ controllers/auth.js
@@ -0,0 +1,13 @@
+const { response } = require('express');
+
+const crearUsuario = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'Crear usuario'
+    });
+}
+
+
+module.exports = {
+    crearUsuario
+}
--- index.js
+++ index.js
@@ -9,6 +9,9 @@ require('./database/config').dbConnection();
 //App de Express
 const app = express();
 
+//Lectura y parseo del Body
+app.use(express.json());
+
 //Node server
 const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
@@ -18,6 +21,9 @@ require('./sockets/socket');
 const publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
+
+//Mis rutas
+app.use('/api/login', require('./routes/auth'));
 
 server.listen(process.env.PORT, (err) => {
     if(err) throw new Error(err);
