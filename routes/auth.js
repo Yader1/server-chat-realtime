ff8d20bb2e16a44f7c05a2ac76de98e9018edc9c
Yader Campbell <yadermejiac123@gmail.com>
2024-08-01 14:41:37 -0600

    feat: Custom middlewares, Validate fields

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,15 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { crearUsuario } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.post('/new', [
-    check('nombre', 'El nombre es obligatorio').not().isEmpty()
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contrasena es obligatorio').not().isEmpty(),
+    check('email', 'El correo es obligatorio').isEmail(),
+    validarCampos
 ], crearUsuario);
 
 module.exports = router;
