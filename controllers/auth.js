850a09459f45c78a1663284b7633e2f521f9008b
Yader Campbell <yadermejiac123@gmail.com>
2024-08-01 19:07:51 -0600

    feat: User model and save to mongoDb

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,11 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 
-const crearUsuario = (req, res = response) => {
+const Usuario = require('../models/usuarios');
+
+const crearUsuario = async (req, res = response) => {
+    const usuario = new Usuario(req.body);
+
+    await usuario.save();
 
     res.json({
         ok: true,
