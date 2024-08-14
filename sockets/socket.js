595c126608e9077e878dd8f4509edefb8436e0e6
Yader Campbell <yadermejiac123@gmail.com>
2024-08-13 23:43:23 -0600

    feat: Validating client if x-token exists in database

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,16 +1,15 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
 
 //Mensajes de Sockets
 io.on('connection', client => {
-    console.log('Cliente conectado');
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+    
+    if(!valido){
+        return client.disconnect();
+    }
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
-
-    /*client.on('mensaje', (payload) => {
-        console.log('Mensaje!', payload);
-
-        io.emit('mensaje', {admin: 'Nuevo mensaje'});
-    });*/
 });
