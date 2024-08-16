4d6535cca5cceb091a730cd0b85a65b457eca6ee
Yader Campbell <yadermejiac123@gmail.com>
2024-08-15 23:14:48 -0600

    feat: Update user data if connected via socket and reflect it in the database

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,15 +1,21 @@
 const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
 
 //Mensajes de Sockets
 io.on('connection', client => {
     const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
     
+    //Veficar autenticacion
     if(!valido){
         return client.disconnect();
     }
 
+    //Cliente autenticado
+    usuarioConectado( uid );
+
+
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        usuarioDesconectado( uid );
     });
 });
